565d355715abef436dfe503e4480910fe8127fa0
Ashutosh Kumar <pathakashutoshdmr@gmail.com>
2026-06-14 21:57:49 +0530

    Allow Vercel URL in CORS origins

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://mentora-mocha-alpha.vercel.app'],
     credentials: true,
 }))
 
